5fa61b0d21db112a838b20956137f825483686a0
robarbms <robjamesbarber@gmail.com>
2024-09-17 07:40:05 -0700

    Seperating out db connection script

--- backend/database/createTables.py
+++ backend/database/createTables.py
@@ -1,6 +1,7 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
+from .connection import connect_to_db
 
 def createTables():
     """ create tables in the PostgreSQL database"""
@@ -38,45 +39,13 @@ def createTables():
         """
     )
 
-    # load environment variables from .env file
-    load_dotenv()
+    cur, conn = connect_to_db()
 
-    db_host = os.getenv('DB_HOST')
-    db_name = os.getenv('DB_NAME')
-    db_user = os.getenv('DB_USER')
-    db_password = os.getenv('DB_PASSWORD')
-    db_port = os.getenv('DB_PORT')
+    # execute the commands using the execute() method of the cursor object
+    for command in commands:
+        cur.execute(command)
 
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(
-            database=db_name,
-            user=db_user,
-            password=db_password,
-            host=db_host,
-            port=db_port,
-        )
-
-        print("Connected to database")
-
-       # create a new cursor object using the cursor() method of the connection object
-        cur = conn.cursor()
-
-       # execute the commands using the execute() method of the cursor object
-        for command in commands:
-            cur.execute(command)
-
-        cur.close() # close the cursor object
-        conn.commit() # commit the changes to the database
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Error: %s" % error)
-
-    # close the database connection using the close() method of the connection object
-    finally:
-        if conn is not None:
-            # close the communication with the PostgreSQL
-            conn.close()
+    cur.close() # close the cursor object
+    conn.commit() # commit the changes to the database
 
 createTables()
